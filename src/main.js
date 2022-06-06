c38079df6cbe4610b6a05eaecaf25b33495cac67
Luis Antonio Rodriguez <larc63@gmail.com>
2022-06-05 21:14:58 -0500

    Added fucntionality to main

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-import Element from './element.js'
+const {MarkDownReader} = require('./mdreader');
+const {YaMdReader} = require('./yamdreader');
+const {HTMLWriter} = require('./writer');
 
+
+const mreader = new MarkDownReader();
+const yreader = new YaMdReader();
+
+exports.getHTML = (mdContent, template) => {
+    console.log(`Contents \n\n ${mdContent}`);
+    const mdTree = mreader.parseText(mdContent);
+    mreader.printTree();
+}
