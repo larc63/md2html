50e5184d71a20c92936673fd7b53a6f7861742d6
Luis Antonio Rodriguez <larc63@gmail.com>
2022-07-05 20:05:55 -0500

    Updated post template

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ const {MarkDownReader} = require('./mdreader');
 const {YaMdReader} = require('./yamdreader');
 const {HTMLWriter} = require('./writer');
 
+const HEADER_KEY = '%%HEADER%%';
 const TITLE_KEY = '%%TITLE%%';
 const BODY_KEY = '%%BODY%%';
+const HERO_KEY = '%%HERO%%';
 
 const mreader = new MarkDownReader();
 const yreader = new YaMdReader();
 const htmlWriter = new HTMLWriter();
 
 const applyTemplate = (p, m, t) => {
-    t = t.replace(TITLE_KEY, `<title>${p.getTitle()}</title>`);
+    t = t.replace(HEADER_KEY, `<title>${p.getTitle()}</title>`);
+    t = t.replace(TITLE_KEY, `${p.getTitle()}`);
     t = t.replace(BODY_KEY, `${htmlWriter.getHTML(m)}`);
+    t = t.replace(HERO_KEY, `<img src="${p.getHero()}">`);
 
     return t;
 }
