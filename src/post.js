17ec54b87186c1e5c3755ebcd150c175e044a42a
Luis Antonio Rodriguez <larc63@gmail.com>
2022-09-15 22:42:37 -0500

    Added linkinbio support

--- src/post.js
+++ src/post.js
@@ -1,12 +1,13 @@
 class Post
  {
-    constructor(title, date, thumb, hero, tags, description){
+    constructor(title, date, thumb, hero, tags, description, linkInBio){
         this.title = title;
         this.date = date;
         this.thumb = thumb;
         this.hero = hero;
         this.tags = tags;
         this.description = description;
+        this.linkInBio = linkInBio;
     }
     getTitle() {
         return this.title;
@@ -25,6 +26,9 @@ class Post
     }
     getTags() {
         return this.tags;
+    }
+    getLinkInBio() {
+        return this.linkInBio;
     }
 }
 exports.Post = Post;
